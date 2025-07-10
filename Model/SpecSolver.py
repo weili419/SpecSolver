5d76610b64c59367104062d5592b066db2bad2fc
Wei Li <107245500+weili419@users.noreply.github.com>
2025-07-10 20:42:25 +0800

    Update SpecSolver.py

--- Model/SpecSolver.py
+++ Model/SpecSolver.py
@@ -232,230 +232,6 @@ class SpaPyBlock(nn.Module):
         y = self.channel(y)*y
         y = self.out(y)   
         return y + x
-    
-class SpePyBlock(nn.Module):
-    def __init__(self, inchannels, bias=True):
-        super(SpePyBlock, self).__init__()
-        self.conv2 = nn.Sequential(
-                        nn.Conv2d(inchannels, inchannels*2, kernel_size=3, stride=1, padding=1, groups=2, bias=bias),
-                        nn.LeakyReLU(0.2),
-                        nn.Conv2d(inchannels*2, inchannels*2, kernel_size=3, stride=1, padding=1, groups=2, bias=bias),
-                        nn.LeakyReLU(0.2)
-                        )
-
-        self.conv4 = nn.Sequential(
-                        nn.Conv2d(inchannels, inchannels*2, kernel_size=3, stride=1, padding=1, groups=4, bias=bias),
-                        nn.LeakyReLU(0.2),
-                        nn.Conv2d(inchannels*2, inchannels*2, kernel_size=3, stride=1, padding=1, groups=4, bias=bias),
-                        nn.LeakyReLU(0.2)
-                        )
-        
-        self.conv8 = nn.Sequential(
-                        nn.Conv2d(inchannels, inchannels*2, kernel_size=3, stride=1, padding=1, groups=8, bias=bias),
-                        nn.LeakyReLU(0.2),
-                        nn.Conv2d(inchannels*2, inchannels*2, kernel_size=3, stride=1, padding=1, groups=8, bias=bias),
-                        nn.LeakyReLU(0.2)
-                        )
-
-    def forward(self, x2,x4,x8):
-        _, c, _, _ = x2.shape
-        if c % 8 != 0:
-            x2 = torch.cat((x2,x2[:,c-(8-c % 8)-1:c-1,:,:]), dim=1)
-            x4 = torch.cat((x4,x4[:,c-(8-c % 8)-1:c-1,:,:]), dim=1)
-            x8 = torch.cat((x8,x8[:,c-(8-c % 8)-1:c-1,:,:]), dim=1)
-        x2_1 = self.conv2(x2)
-        x4_1 = self.conv4(x4)
-        x8_1 = self.conv8(x8)
-
-        return x2_1,x4_1,x8_1
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2),
-
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.fn = fn
-        self.norm = nn.LayerNorm(dim)
-
-    def forward(self, x, *args, **kwargs):
-        x = self.norm(x)
-        return self.fn(x, *args, **kwargs)
-
-class GELU(nn.Module):
-    def forward(self, x):
-        return F.gelu(x)
-
-class MSA(nn.Module):
-    def __init__(
-            self,
-            dim,
-            dim_head,
-            heads,
-    ):
-        super().__init__()
-        self.num_heads = heads
-        self.dim_head = dim_head
-        self.to_qm = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_km = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_vm = nn.Linear(dim, dim_head * heads, bias=False)        
-        self.to_k2 = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_v2 = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_k4 = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_v4 = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_k8 = nn.Linear(dim, dim_head * heads, bias=False)
-        self.to_v8 = nn.Linear(dim, dim_head * heads, bias=False)
-        self.rescalem = nn.Parameter(torch.ones(heads, 1, 1))
-        self.rescale2 = nn.Parameter(torch.ones(heads, 1, 1))
-        self.rescale4 = nn.Parameter(torch.ones(heads, 1, 1))
-        self.rescale8 = nn.Parameter(torch.ones(heads, 1, 1))
-        self.proj = nn.Linear(dim_head * heads, dim, bias=True)
-        self.pos_emb = nn.Sequential(
-            nn.Conv2d(dim, dim, 3, 1, 1, bias=False, groups=dim),
-            GELU(),
-            nn.Conv2d(dim, dim, 3, 1, 1, bias=False, groups=dim),
-        )
-        self.dim = dim
-
-    def forward(self, x_2,x_4,x_8,y):
-        """
-        x_in: [b,h,w,c]
-        return out: [b,h,w,c]
-        """
-        b, h, w, c = y.shape
-        x2 = x_2.reshape(b,h*w,c)
-        x4 = x_4.reshape(b,h*w,c)
-        x8 = x_8.reshape(b,h*w,c)
-        y = y.reshape(b,h*w,c)
-
-        q_inpm = self.to_qm(y)
-        k_inpm = self.to_km(y)
-        v_inpm = self.to_vm(y)
-        qm, km, vm = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads),
-                                (q_inpm, k_inpm, v_inpm))
-        vm = vm
-        # q: b,heads,hw,c
-        qm = qm.transpose(-2, -1)
-        km = km.transpose(-2, -1)
-        vm = vm.transpose(-2, -1)
-        qm = F.normalize(qm, dim=-1, p=2)
-        km = F.normalize(km, dim=-1, p=2)
-        attnm = (km @ qm.transpose(-2, -1))   # A = K^T*Q
-        attnm = attnm * self.rescalem
-        attnm = attnm.softmax(dim=-1)
-
-        k_inp2 = self.to_k2(x2)
-        v_inp2 = self.to_v2(x2)
-        k2, v2 = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads),
-                                (k_inp2, v_inp2))
-        v2 = v2
-        # q: b,heads,hw,c
-        k2 = k2.transpose(-2, -1)
-        v2 = v2.transpose(-2, -1)
-        k2 = F.normalize(k2, dim=-1, p=2)
-        attn2 = (k2 @ qm.transpose(-2, -1))   # A = K^T*Q
-        attn2 = attn2 * self.rescale2
-        attn2 = attn2.softmax(dim=-1)
-
-        k_inp4 = self.to_k4(x4)
-        v_inp4 = self.to_v4(x4)
-        k4, v4 = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads),
-                                (k_inp4, v_inp4))
-        v4 = v4
-        # q: b,heads,hw,c
-        k4 = k4.transpose(-2, -1)
-        v4 = v4.transpose(-2, -1)
-        k4 = F.normalize(k4, dim=-1, p=2)
-        attn4 = (k4 @ qm.transpose(-2, -1))   # A = K^T*Q
-        attn4 = attn4 * self.rescale4
-        attn4 = attn4.softmax(dim=-1)
-
-        k_inp8 = self.to_k8(x8)
-        v_inp8 = self.to_v8(x8)
-        k8, v8 = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads),
-                                (k_inp8, v_inp8))
-        v8 = v8
-        # q: b,heads,hw,c
-        k8 = k8.transpose(-2, -1)
-        v8 = v8.transpose(-2, -1)
-        k8 = F.normalize(k8, dim=-1, p=2)
-        attn8 = (k8 @ qm.transpose(-2, -1))   # A = K^T*Q
-        attn8 = attn8 * self.rescale4
-        attn8 = attn8.softmax(dim=-1)
-
-        x = attnm @ vm +  attn2 @ v2 + attn4 @ v4 + attn8 @ v8  # b,heads,d,hw
-        x = x.permute(0, 3, 1, 2)    # Transpose
-        x = x.reshape(b, h * w, self.num_heads * self.dim_head)
-        out_c = self.proj(x).view(b, h, w, c)
-        out_p = self.pos_emb(v_inpm.reshape(b,h,w,c).permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
-        out = out_c + out_p
-
-        return out
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, mult=4):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(dim, dim * mult, 1, 1, bias=False),
-            GELU(),
-            nn.Conv2d(dim * mult, dim * mult, 3, 1, 1, bias=False, groups=dim * mult),
-            GELU(),
-            nn.Conv2d(dim * mult, dim, 1, 1, bias=False),
-        )
-
-    def forward(self, x):
-        """
-        x: [b,h,w,c]
-        return out: [b,h,w,c]
-        """
-        out = self.net(x.permute(0, 3, 1, 2))
-        return out.permute(0, 2, 3, 1)
-    
-class MLSIF(nn.Module):
-    def __init__(
-            self,
-            dim,
-            dim_head,
-            heads,
-            num_blocks,
-    ):
-        super().__init__()
-        self.blocks = nn.ModuleList([])
-        for _ in range(num_blocks):
-            self.blocks.append(nn.ModuleList([
-                MSA(dim=dim, dim_head=dim_head, heads=heads),
-                PreNorm(dim, FeedForward(dim=dim))
-            ]))
-
-    def forward(self, x_2,x_4,x_8,y):
-        """
-        x: [b,c,h,w]
-        return out: [b,c,h,w]
-        """
-        x_2 = x_2.permute(0, 2, 3, 1)
-        x_4 = x_4.permute(0, 2, 3, 1)
-        x_8 = x_8.permute(0, 2, 3, 1)
-        y = y.permute(0, 2, 3, 1)
-        for (attn, ff) in self.blocks:
-            x = attn(x_2,x_4,x_8,y) + y
-            x = ff(x) + x
-        out = x.permute(0, 3, 1, 2)
-        return out
        
 #################################################################################################################################    
 class MLP_3(nn.Module):
@@ -524,11 +300,7 @@ class MLP(nn.Module):
         x = self.linear_post(x)
         return x
 
-
-
-
-class Specsolver_block(nn.Module):
-    """Transformer encoder block."""
+class Transolver_block(nn.Module):
 
     def __init__(
             self,
@@ -609,7 +381,7 @@ class Modelcave(nn.Module):
         ##########################################################################################################################
         self.preprocess = MLP(fun_dim + 2, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act)
 
-        self.blocks = nn.ModuleList([Specsolver_block(num_heads=n_head, hidden_dim=n_hidden,
+        self.blocks = nn.ModuleList([Transolver_block(num_heads=n_head, hidden_dim=n_hidden,
                                                       dropout=dropout,
                                                       act=act,
                                                       mlp_ratio=mlp_ratio,
@@ -708,7 +480,7 @@ class Modelharvard(nn.Module):
         ##########################################################################################################################
         self.preprocess = MLP(fun_dim + 2, n_hidden * 2, n_hidden, n_layers=0, res=False, act=act)
 
-        self.blocks = nn.ModuleList([Specsolver_block(num_heads=n_head, hidden_dim=n_hidden,
+        self.blocks = nn.ModuleList([Transolver_block(num_heads=n_head, hidden_dim=n_hidden,
                                                       dropout=dropout,
                                                       act=act,
                                                       mlp_ratio=mlp_ratio,
